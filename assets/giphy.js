8cd99d9d080c164669a9174d9df8370f8669dd3c
Real Ghost <alejandro519@gmail.com>
2019-09-11 23:56:26 -0700

    Fixed a minor error in JS for an HTML element

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,12 +1,9 @@
 var presidents = ["Barack Obama", "George W. Bush", "Bill Clinton", "Jimmy Carter"];
 
-
 function displayPresidentInfo() {
 
   var president = $(this).attr("data-name");
   var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=ugWOWOZbOIs5AcEvCHRPRixd5gWW10Rq&q=" + president + "&limit=5&offset=0&rating=PG-13&lang=en";
-
-
 
   $.ajax({
     url: queryUrl,
@@ -28,11 +25,10 @@ function displayPresidentInfo() {
 
     presDiv.append(image);
 
-    $("presidential-view").prepend(presDiv);
+    $("#presidential-view").prepend(presDiv);
   });
 
 };
-
 
 function renderButtons() {
 
